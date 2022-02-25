9a49d31f521342efee62d5788dfb81332ec5e8e5
LuKaz <andresfeliperu@gmail.com>
2022-02-25 11:35:30 +0000

    stuffs :

--- src/pages/{DictJson.lang}/contact/index.jsx
+++ src/pages/{DictJson.lang}/contact/index.jsx
@@ -18,8 +18,8 @@ export default function Contact({params}) {
                   method="POST"
                   action={postTo}
                   data-netlify="true"
-                  // netlify-honeypot="bot-field"
-                  // data-netlify-recaptcha="true"
+                  netlify-honeypot="bot-field"
+                  data-netlify-recaptcha="true"
                 >
                     <input type="hidden" name="form-name" value="contact"/>
                     <div className="overflow-hidden ">
